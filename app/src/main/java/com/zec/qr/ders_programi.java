dbabe2947a975f88cba8e046d707d65dbc577fcf
Dilek Karataş <dilekaratas@ya.ru>
2015-08-07 00:48:55 +0300

    son123

--- app/src/main/java/com/zec/qr/ders_programi.java
+++ app/src/main/java/com/zec/qr/ders_programi.java
@@ -1,12 +1,18 @@
 package com.zec.qr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ders_programi extends AppCompatActivity {
 
+    public void home_go(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
